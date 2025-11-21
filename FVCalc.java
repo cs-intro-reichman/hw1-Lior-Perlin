974014473830a2ab860a84c2e30be6ee314554e7
Lior Perlin <lior.perlin@post.runi.ac.il>
2025-11-21 13:25:50 +0200

    Completed FVCalc.java Assaignment

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,11 @@
 // Computes the future value of a saving investment.
-public class FVCalc {
+public class FVCalc {java FVCalc 25000 6 25
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int curValue = Integer.parseInt(args[0]);
+		double interRate = Double.parseDouble(args[1])/100;
+		int years = Integer.parseInt(args[2]);
+		double futValue = curValue * Math.pow((1 + interRate), years);
+		interRate *= 100;
+		System.out.println("After " + years + " years, $" + curValue +" saved at " + interRate + "% will yield $" + (int)futValue);
 	}
 }
