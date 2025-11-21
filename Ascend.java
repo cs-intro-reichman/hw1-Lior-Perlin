6e56aff2db9f7b987c5a59faa15c195a55dcc676
Lior Perlin <lior.perlin@post.runi.ac.il>
2025-11-21 14:49:11 +0200

    Finished Ascend.java Assaignment

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,14 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int limit = Integer.parseInt(args[0]);
+		int a = (int)(Math.random()*(double)limit);
+		int b = (int)(Math.random()*(double)limit);
+		int c = (int)(Math.random()*(double)limit);
+		int min = Math.min(Math.min(a,b),c);
+		int max = Math.max(Math.max(a,b),c);
+		int balance = Math.max(Math.min ( Math.max(a,b),c) ,Math.min ( Math.max(a,c),b));
+		System.out.println(max + " " + balance + " " + min);
+		System.out.println(min + " " + balance + " " + max);
 	}
 }
