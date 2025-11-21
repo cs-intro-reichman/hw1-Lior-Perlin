20cb27fdd2e919be35e8aca649f15e9a8b36cc52
Lior Perlin <lior.perlin@post.runi.ac.il>
2025-11-21 15:42:07 +0200

    Addition of comments to all of programs

--- Ascend.java
+++ Ascend.java
@@ -3,13 +3,14 @@
 public class Ascend {
 	public static void main(String[] args) {
 		int limit = Integer.parseInt(args[0]);
+		// Generating 3 random numbers and making sure they are in the range
 		int a = (int)(Math.random()*(double)limit);
 		int b = (int)(Math.random()*(double)limit);
 		int c = (int)(Math.random()*(double)limit);
-		int min = Math.min(Math.min(a,b),c);
-		int max = Math.max(Math.max(a,b),c);
-		int balance = Math.max(Math.min ( Math.max(a,b),c) ,Math.min ( Math.max(a,c),b));
-		System.out.println(max + " " + balance + " " + min);
-		System.out.println(min + " " + balance + " " + max);
+		int min = Math.min(Math.min(a,b),c); // Checking the minimal number
+		int max = Math.max(Math.max(a,b),c); // Checking the maximum number
+		int balance = Math.max(Math.min ( Math.max(a,b),c) ,Math.min ( Math.max(a,c),b)); // Checking for the balanced number
+		System.out.println(max + " " + balance + " " + min); // Outprint Max to Min
+		System.out.println(min + " " + balance + " " + max); // Outprint Min to Max
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -2,9 +2,9 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    int num = Integer.parseInt(args[0]);
-		int hundreds = num / 100;
-		num %= 100;
-		int tens = num / 10;
+		// Calculating the terms of hundreds, tens, and ones
+		int hundreds = num / 100; 
+		int tens = (num / 10) % 10;
 		int ones = num % 10;
 		System.out.println(hundreds + " hundreds, " + tens +" tens, and " + ones +" ones.");
 	}
